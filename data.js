052967d0c805dc5e86e14ff02410bd641c941d6f
piaisami <piayelps@gmail.com>
2025-07-13 15:21:47 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,80 +1,70 @@
 // Todos los ramos, créditos, prerrequisitos y semestre
-const ramos = [
-  // PRIMER AÑO
-  // Primer semestre
-  { id: "quimica_general", nombre: "Química General", creditos: 4, requiere: [], semestre: "Primer Año - Primer Semestre" },
-  { id: "matematicas", nombre: "Matemáticas", creditos: 8, requiere: [], semestre: "Primer Año - Primer Semestre" },
-  { id: "biologia_celular", nombre: "Biología Celular", creditos: 6, requiere: [], semestre: "Primer Año - Primer Semestre" },
-  { id: "intro_laboratorio", nombre: "Introducción al Laboratorio", creditos: 3, requiere: [], semestre: "Primer Año - Primer Semestre" },
-  { id: "intro_quimica_farmacia", nombre: "Introducción a la Química y Farmacia", creditos: 6, requiere: [], semestre: "Primer Año - Primer Semestre" },
-  { id: "anatomia", nombre: "Anatomía", creditos: 3, requiere: [], semestre: "Primer Año - Primer Semestre" },
+  const ramos = [
+  // Primer año
+  { id: "quimica_general", nombre: "Química General", creditos: 4, requiere: [], year: 1, sem: 1 },
+  { id: "matematicas", creditos: 8, requiere: [], year: 1, sem: 1 },
+  { id: "biologia_celular", creditos: 6, requiere: [], year: 1, sem: 1 },
+  { id: "intro_lab", creditos: 3, requiere: [], year: 1, sem: 1 },
+  { id: "intro_qf", creditos: 6, requiere: [], year: 1, sem: 1 },
+  { id: "anatomia", creditos: 3, requiere: [], year: 1, sem: 1 },
 
-  // Segundo semestre
-  { id: "quimica_organica_i", nombre: "Química Orgánica I", creditos: 6, requiere: ["quimica_general", "intro_laboratorio"], semestre: "Primer Año - Segundo Semestre" },
-  { id: "fisicoquimica", nombre: "Fisicoquímica", creditos: 8, requiere: ["quimica_general", "matematicas"], semestre: "Primer Año - Segundo Semestre" },
-  { id: "calculo", nombre: "Cálculo", creditos: 4, requiere: ["matematicas"], semestre: "Primer Año - Segundo Semestre" },
-  { id: "botanica", nombre: "Botánica", creditos: 4, requiere: ["biologia_celular"], semestre: "Primer Año - Segundo Semestre" },
-  { id: "bioestadistica", nombre: "Bioestadística", creditos: 4, requiere: ["matematicas"], semestre: "Primer Año - Segundo Semestre" },
-  { id: "tnl", nombre: "TNL", creditos: 2, requiere: [], semestre: "Primer Año - Segundo Semestre" },
+  { id: "quim_organic_i", creditos: 6, requiere: ["quimica_general", "intro_lab"], year: 1, sem: 2 },
+  { id: "fisicoquimica", creditos: 8, requiere: ["quimica_general", "matematicas"], year: 1, sem: 2 },
+  { id: "calculo", creditos: 4, requiere: ["matematicas"], year: 1, sem: 2 },
+  { id: "botanica", creditos: 4, requiere: ["biologia_celular"], year: 1, sem: 2 },
+  { id: "bioestadistica", creditos: 4, requiere: ["matematicas"], year: 1, sem: 2 },
+  { id: "tnl", creditos: 2, requiere: [], year: 1, sem: 2 },
 
-  // SEGUNDO AÑO
-  // Primer semestre
-  { id: "quimica_organica_ii", nombre: "Química Orgánica II", creditos: 8, requiere: ["quimica_organica_i"], semestre: "Segundo Año - Primer Semestre" },
-  { id: "quimica_analitica", nombre: "Química Analítica", creditos: 8, requiere: ["fisicoquimica"], semestre: "Segundo Año - Primer Semestre" },
-  { id: "fisiologia_i", nombre: "Fisiología I", creditos: 6, requiere: ["biologia_celular", "anatomia"], semestre: "Segundo Año - Primer Semestre" },
-  { id: "tni", nombre: "TNI", creditos: 2, requiere: [], semestre: "Segundo Año - Primer Semestre" },
-  { id: "bioquimica", nombre: "Bioquímica", creditos: 6, requiere: ["biologia_celular", "quimica_organica_i"], semestre: "Segundo Año - Primer Semestre" },
+  // Segundo año
+  { id: "quim_organic_ii", creditos: 8, requiere: ["quim_organic_i"], year: 2, sem: 1 },
+  { id: "quim_analitica", creditos: 8, requiere: ["fisicoquimica"], year: 2, sem: 1 },
+  { id: "fisiologia_i", creditos: 6, requiere: ["biologia_celular", "anatomia"], year: 2, sem: 1 },
+  { id: "tni", creditos: 2, requiere: [], year: 2, sem: 1 },
+  { id: "bioquimica", creditos: 6, requiere: ["biologia_celular", "quim_organic_i"], year: 2, sem: 1 },
 
-  // Segundo semestre
-  { id: "farmacognosia", nombre: "Farmacognosia", creditos: 6, requiere: ["botanica", "quimica_organica_ii"], semestre: "Segundo Año - Segundo Semestre" },
-  { id: "analisis_instrumental", nombre: "Análisis Instrumental", creditos: 8, requiere: ["quimica_analitica"], semestre: "Segundo Año - Segundo Semestre" },
-  { id: "operaciones_unitarias", nombre: "Operaciones Unitarias", creditos: 5, requiere: ["fisicoquimica"], semestre: "Segundo Año - Segundo Semestre" },
-  { id: "fisiologia_ii", nombre: "Fisiología II", creditos: 4, requiere: ["fisiologia_i"], semestre: "Segundo Año - Segundo Semestre" },
-  { id: "administracion", nombre: "Administración de Organizaciones", creditos: 5, requiere: ["matematicas", "tnl"], semestre: "Segundo Año - Segundo Semestre" },
-  { id: "intro_quimica_farmaceutica", nombre: "Introducción a la química farmacéutica", creditos: 2, requiere: ["quimica_organica_ii"], semestre: "Segundo Año - Segundo Semestre" },
+  { id: "farmacognosia", creditos: 6, requiere: ["botanica", "quim_organic_ii"], year: 2, sem: 2 },
+  { id: "analisis_instr", creditos: 8, requiere: ["quim_analitica"], year: 2, sem: 2 },
+  { id: "oper_unit", creditos: 5, requiere: ["fisicoquimica"], year: 2, sem: 2 },
+  { id: "fisiologia_ii", creditos: 4, requiere: ["fisiologia_i"], year: 2, sem: 2 },
+  { id: "administracion", creditos: 5, requiere: ["matematicas", "tnl"], year: 2, sem: 2 },
+  { id: "intro_qfarm", creditos: 2, requiere: ["quim_organic_ii"], year: 2, sem: 2 },
 
-  // TERCER AÑO
-  // Primer semestre
-  { id: "etica", nombre: "Ética", creditos: 2, requiere: ["tnl"], semestre: "Tercer Año - Primer Semestre" },
-  { id: "administracion_rrhh", nombre: "Administración de Recursos Humanos", creditos: 3, requiere: ["administracion"], semestre: "Tercer Año - Primer Semestre" },
-  { id: "biologia_molecular", nombre: "Biología Molecular", creditos: 4, requiere: ["bioquimica"], semestre: "Tercer Año - Primer Semestre" },
-  { id: "fisiopatologia", nombre: "Fisiopatología", creditos: 4, requiere: ["fisiologia_ii"], semestre: "Tercer Año - Primer Semestre" },
-  { id: "quimica_farmaceutica_i", nombre: "Química Farmacéutica I", creditos: 6, requiere: ["intro_quimica_farmaceutica", "fisiologia_ii"], semestre: "Tercer Año - Primer Semestre" },
-  { id: "electivo_i", nombre: "Electivo I", creditos: 2, requiere: [], semestre: "Tercer Año - Primer Semestre" },
-  { id: "microbiologia", nombre: "Microbiología", creditos: 5, requiere: ["bioquimica"], semestre: "Tercer Año - Primer Semestre" },
-  { id: "legislacion", nombre: "Legislación Farmacéutica", creditos: 2, requiere: ["administracion"], semestre: "Tercer Año - Primer Semestre" },
+  // Tercer año
+  { id: "etica", creditos: 2, requiere: ["tnl"], year: 3, sem: 1 },
+  { id: "admin_rrhh", creditos: 3, requiere: ["administracion"], year: 3, sem: 1 },
+  { id: "bio_molecular", creditos: 4, requiere: ["bioquimica"], year: 3, sem: 1 },
+  { id: "fisiopato", creditos: 4, requiere: ["fisiologia_ii"], year: 3, sem: 1 },
+  { id: "quimfarm_i", creditos: 6, requiere: ["intro_qfarm", "fisiologia_ii"], year: 3, sem: 1 },
+  { id: "electivo_i", creditos: 2, requiere: [], year: 3, sem: 1 },
+  { id: "microbiologia", creditos: 5, requiere: ["bioquimica"], year: 3, sem: 1 },
+  { id: "legislacion", creditos: 2, requiere: ["administracion"], year: 3, sem: 1 },
 
-  // Segundo semestre
-  { id: "biofarmacia", nombre: "Biofarmacia", creditos: 8, requiere: ["analisis_instrumental", "fisiologia_ii"], semestre: "Tercer Año - Segundo Semestre" },
-  { id: "farmacologia_i", nombre: "Farmacología I", creditos: 7, requiere: ["quimica_farmaceutica_i", "fisiopatologia"], semestre: "Tercer Año - Segundo Semestre" },
-  { id: "inmunologia", nombre: "Inmunología", creditos: 4, requiere: ["biologia_molecular", "fisiopatologia"], semestre: "Tercer Año - Segundo Semestre" },
-  { id: "quimica_farmaceutica_ii", nombre: "Química Farmacéutica II", creditos: 6, requiere: ["quimica_farmaceutica_i", "microbiologia"], semestre: "Tercer Año - Segundo Semestre" },
-  { id: "practica_profesional_i", nombre: "Práctica Profesional I", creditos: 7, requiere: ["etica", "administracion_rrhh", "legislacion"], semestre: "Tercer Año - Segundo Semestre" },
+  { id: "biofarmacia", creditos: 8, requiere: ["analisis_instr", "fisiologia_ii"], year: 3, sem: 2 },
+  { id: "farmacologia_i", creditos: 7, requiere: ["quimfarm_i", "fisiopato"], year: 3, sem: 2 },
+  { id: "inmunologia", creditos: 4, requiere: ["bio_molecular", "fisiopato"], year: 3, sem: 2 },
+  { id: "quimfarm_ii", creditos: 6, requiere: ["quimfarm_i", "microbiologia"], year: 3, sem: 2 },
+  { id: "pp_i", creditos: 7, requiere: ["etica", "admin_rrhh", "legislacion"], year: 3, sem: 2 },
 
-  // CUARTO AÑO
-  // Primer semestre
-  { id: "tecnologia_farmaceutica_i", nombre: "Tecnología Farmacéutica I", creditos: 7, requiere: ["operaciones_unitarias", "biofarmacia"], semestre: "Cuarto Año - Primer Semestre" },
-  { id: "farmacologia_ii", nombre: "Farmacología II", creditos: 8, requiere: ["farmacologia_i"], semestre: "Cuarto Año - Primer Semestre" },
-  { id: "quimica_fisiologica_patologica", nombre: "Química Fisiológica y Patológica", creditos: 5, requiere: ["inmunologia"], semestre: "Cuarto Año - Primer Semestre" },
-  { id: "salud_publica", nombre: "Salud Pública", creditos: 4, requiere: ["etica"], semestre: "Cuarto Año - Primer Semestre" },
-  { id: "quimica_de_alimentos", nombre: "Química de Alimentos", creditos: 4, requiere: ["analisis_instrumental"], semestre: "Cuarto Año - Primer Semestre" },
-  { id: "tip1", nombre: "TIP1", creditos: 2, requiere: ["farmacologia_i", "practica_profesional_i"], semestre: "Cuarto Año - Primer Semestre" },
+  // Cuarto año
+  { id: "tecnol_farm_i", creditos: 7, requiere: ["oper_unit", "biofarmacia"], year: 4, sem: 1 },
+  { id: "farmacologia_ii", creditos: 8, requiere: ["farmacologia_i"], year: 4, sem: 1 },
+  { id: "quim_fisio_pat", creditos: 5, requiere: ["inmunologia"], year: 4, sem: 1 },
+  { id: "salud_publica", creditos: 4, requiere: ["etica"], year: 4, sem: 1 },
+  { id: "quim_alimentos", creditos: 4, requiere: ["analisis_instr"], year: 4, sem: 1 },
+  { id: "tip1", creditos: 2, requiere: ["farmacologia_i", "pp_i"], year: 4, sem: 1 },
 
-  // Segundo semestre
-  { id: "tecnologia_farmaceutica_ii", nombre: "Tecnología Farmacéutica II", creditos: 7, requiere: ["tecnologia_farmaceutica_i", "biofarmacia"], semestre: "Cuarto Año - Segundo Semestre" },
-  { id: "farmacia_asistencial", nombre: "Farmacia Asistencial", creditos: 3, requiere: ["farmacologia_ii"], semestre: "Cuarto Año - Segundo Semestre" },
-  { id: "metodologia_investigacion", nombre: "Metodología de la Investigación", creditos: 2, requiere: [], creditos_requiere: 178, semestre: "Cuarto Año - Segundo Semestre" },
-  { id: "nutricion", nombre: "Nutrición", creditos: 4, requiere: ["quimica_fisiologica_patologica", "quimica_de_alimentos"], semestre: "Cuarto Año - Segundo Semestre" },
-  { id: "tip2", nombre: "TIP2", creditos: 2, requiere: ["tip1"], semestre: "Cuarto Año - Segundo Semestre" },
+  { id: "tecnol_farm_ii", creditos: 7, requiere: ["tecnol_farm_i", "biofarmacia"], year: 4, sem: 2 },
+  { id: "farm_asistencial", creditos: 3, requiere: ["farmacologia_ii"], year: 4, sem: 2 },
+  { id: "metodologia_invest", creditos: 2, requiere: [], creditos_requiere: 178, year: 4, sem: 2 },
+  { id: "nutricion", creditos: 4, requiere: ["quim_fisio_pat", "quim_alimentos"], year: 4, sem: 2 },
+  { id: "tip2", creditos: 2, requiere: ["tip1"], year: 4, sem: 2 },
 
-  // QUINTO AÑO
-  // Primer semestre
-  { id: "farmacovigilancia", nombre: "Farmacovigilancia", creditos: 2, requiere: ["farmacia_asistencial", "farmacologia_ii"], semestre: "Quinto Año - Primer Semestre" },
-  { id: "farmacia_clinica", nombre: "Farmacia Clínica", creditos: 4, requiere: ["farmacia_asistencial", "metodologia_investigacion"], semestre: "Quinto Año - Primer Semestre" },
-  { id: "toxicologia", nombre: "Toxicología", creditos: 4, requiere: ["farmacologia_ii"], semestre: "Quinto Año - Primer Semestre" },
-  { id: "quimica_farmaceutica_iii", nombre: "Química Farmacéutica III", creditos: 5, requiere: ["quimica_farmaceutica_ii"], semestre: "Quinto Año - Primer Semestre" },
-  { id: "electivo_ii", nombre: "Electivo II", creditos: 2, requiere: [], semestre: "Quinto Año - Primer Semestre" },
+  // Quinto año
+  { id: "farmacov", creditos: 2, requiere: ["farm_asistencial", "farmacologia_ii"], year: 5, sem: 1 },
+  { id: "farm_clinica", creditos: 4, requiere: ["farm_asistencial", "metodologia_invest"], year: 5, sem: 1 },
+  { id: "toxicologia", creditos: 4, requiere: ["farmacologia_ii"], year: 5, sem: 1 },
+  { id: "quimfarm_iii", creditos: 5, requiere: ["quimfarm_ii"], year: 5, sem: 1 },
+  { id: "electivo_ii", creditos: 2, requiere: [], year: 5, sem: 1 },
 
-  // Segundo semestre
-  { id: "ui_internado_i", nombre: "UI / Internado I", creditos: 5, requiere: ["farmacia_clinica", "farmacovigilancia", "quimica_farmaceutica_iii"], semestre: "Quinto Año - Segundo Semestre" },
+  { id: "ui_internado_i", creditos: 5, requiere: ["farm_clinica", "farmacov", "quimfarm_iii"], year: 5, sem: 2 },
 ];
